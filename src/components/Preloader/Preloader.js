f5ec1cee2f84537519189fe9eacbe47e951ec911
Anas Shaikh <shaikhanas246@gmail.com>
2021-05-29 18:38:31 +0530

    specified image height

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,33 +1,40 @@
-import {PropTypes} from 'prop-types';
-import './Preloader.css'
+import { PropTypes } from "prop-types";
+import "./Preloader.css";
 
-export const Preloader = ({loading,image,spinnerColor,spinnerSize}) =>{
-  
-    if(loading){
-        return (
-        <div className="preloader">
-            {image ? <img src={image} alt="" /> : 
-            ( 
-            <div class="loader-indicator" 
-                style={{borderTopColor:spinnerColor,height:`${spinnerSize}px`,width:`${spinnerSize}px`}}>
-            </div>
-            ) 
-            }
-            {/* TODO : Add loading UI  */}
-        </div>)
-    }
-    else{
-        return null
-    }
-
-
-    
-}
-
+export const Preloader = ({
+  loading,
+  image,
+  spinnerColor,
+  spinnerSize,
+  imageSize,
+}) => {
+  if (loading) {
+    return (
+      <div className="preloader">
+        {image ? (
+          <img src={image} alt="" style={{ height: imageSize }} />
+        ) : (
+          <div
+            class="loader-indicator"
+            style={{
+              borderTopColor: spinnerColor,
+              height: `${spinnerSize}px`,
+              width: `${spinnerSize}px`,
+            }}
+          ></div>
+        )}
+        {/* TODO : Add loading UI  */}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
 Preloader.propTypes = {
-    loading : PropTypes.bool,
-    image : PropTypes.String,
-    spinnerColor: PropTypes.Color,
-    spinnerSize : PropTypes.Number,
-}
+  loading: PropTypes.bool,
+  image: PropTypes.String,
+  spinnerColor: PropTypes.Color,
+  spinnerSize: PropTypes.Number,
+  imageSize: PropTypes.Number,
+};
